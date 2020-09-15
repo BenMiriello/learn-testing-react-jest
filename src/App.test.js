755a50d0bea8494aad1cfe04cbb02d635235f6ee
BenMiriello <benmiriello@gmail.com>
2020-09-15 17:45:32 -0400

    First test and snapshot with react-testing-library

--- src/App.test.js
+++ src/App.test.js
@@ -1,48 +1,13 @@
 import React from 'react';
-// import { render } from '@testing-library/react';
+import { render, cleanup } from '@testing-library/react';
 import App from './App';
-import Account from './Account';
-import { configure, shallow, mount } from 'enzyme';
-import Adapter from "enzyme-adapter-react-16";
-import toJson from "enzyme-to-json";
-configure({ adapter: new Adapter() });
 
+afterEach(cleanup);
 
-it("renders without crashing", () => {
-  shallow(<App />);
-});
-
-it("renders Account header", () => {
-  const wrapper = shallow(<App />);
-  const welcome = <h1>Display Active Users Account Details</h1>;
-  expect(wrapper.contains(welcome)).toEqual(true);
-});
-
-const user = {
-  name: "Adeneye David",
-  email: "david@gmail.com",
-  username: "Dave",
-};
-
-describe("", () => {
-  it("accepts user account props", () => {
-    const wrapper = mount(<Account user={user} />);
-    expect(wrapper.props().user).toEqual(user);
+describe('App', () => {
+  it('should take a snapshot', () => {
+    const { asFragment } = render(<App />);
+  
+    expect(asFragment(<App />)).toMatchSnapshot();
   })
-
-  it("contains users account email", () => {
-    const wrapper = mount(<Account user={user} />);
-    const value = wrapper.find("p").text();
-    expect(value).toEqual("david@gmail.com");
-  })
-
-  it("renders correctly with no error message", () => {
-    const wrapper = mount(<App />);
-    expect(wrapper.state("error")).toEqual(null);
-  })
-});
-
-it("renders correctly", () => {
-  const tree = shallow(<App />);
-  expect(toJson(tree)).toMatchSnapshot();
 });
