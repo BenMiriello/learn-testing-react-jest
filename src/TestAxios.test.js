6ea66a9326219a864ee87bd1fdebcfb6d23a06c8
BenMiriello <benmiriello@gmail.com>
2020-09-22 22:51:33 -0400

    Add secoond axios test, passing

--- src/TestAxios.test.js
+++ src/TestAxios.test.js
@@ -10,3 +10,18 @@ it('should display a loading text', () => {
 
     expect(getByTestId('loading')).toHaveTextContent('Loading...')
 });
+
+it('should load and display the data', async () => {
+    const { getByTestId } = render(<TestAxios url={'/greeting'} />);
+    axiosMock.get.mockResolvedValueOnce({
+        data: { greeting: 'hello there' },
+    });
+
+    fireEvent.click(getByTestId('fetch-data'));
+
+    const greetingData = await waitForElement(() => getByTestId('show-data'));
+
+    expect(axiosMock.get).toHaveBeenCalledTimes(1);
+    expect(axiosMock.get).toHaveBeenCalledWith(url);
+    expect(GreetingData).toHaveTextContent('hello there');
+});
